f04187779bba6bfd5472070511462c86f5fdcd2b
shreshthUnderscore <88784050+shreshthUnderscore@users.noreply.github.com>
2025-02-28 00:48:16 +0000

    lalallaa

--- backend/app/routers/__init__.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from . import users, resume, kanban, assistant
-
-main_router = APIRouter(prefix="/v1")
-
-main_router.include_router(users.router)
-main_router.include_router(resume.router)
-main_router.include_router(kanban.router)
-main_router.include_router(assistant.router)
